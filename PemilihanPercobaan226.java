0a5ad0bad173752981a14fe2b6c9484d280c853c
SatriaNadirakhma <144087759+SatriaNadirakhma@users.noreply.github.com>
2023-09-26 11:58:10 +0700

    Commit Change
    Satria Rakhmadani Percobaan 2

---
+++ PemilihanPercobaan226.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan226 {
+    public static void main(String[] args) {
+        Scanner input26 = new Scanner(System.in);
+
+        System.out.print("Nilai uas     : ");
+        float uas = input26.nextFloat();
+        System.out.print("Nilai uts     : ");
+        float uts = input26.nextFloat();
+        System.out.print("Nilai kuis    : ");
+        float kuis = input26.nextFloat();
+        System.out.print("Nilai Tugas   : ");
+        float tugas = input26.nextFloat();
+
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+        String message = total < 65 ? "Remidi" : "Tidak remidi";
+        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+    }
+}
