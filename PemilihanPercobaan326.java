1ba7486213d2de20406c69a0822c4fa8df23b9d4
SatriaNadirakhma <144087759+SatriaNadirakhma@users.noreply.github.com>
2023-09-26 14:55:05 +0700

    Pemilihan Percobaan 326 Commit Change
    Satria Rakhmadani Commit Change

---
+++ PemilihanPercobaan326.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class PemilihanPercobaan326 {
+    public static void main(String[] args) {
+        Scanner input26 = new Scanner(System.in);
+
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.println("Masukkan Angka Pertama: ");
+        angka1 = input26.nextDouble();
+        System.out.println("Masukkan Angka Kedua: ");
+        angka2 = input26.nextDouble();
+        System.out.println("Masukkan Operator (+ - * /): ");
+        operator = input26.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                System.out.println(angka1 + " + " + angka2 + "=" + hasil);
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                System.out.println(angka1 + " - " + angka2 + "=" + hasil);
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                System.out.println(angka1 + " * " + angka2 + "=" + hasil);
+                break;
+            case '/':
+                hasil = angka1 / angka2;
+                System.out.println(angka1 + " / " + angka2 + "=" + hasil);
+                break;
+        }
+
+    }
+}
